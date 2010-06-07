9a04b256f002d707c8f87d71dfcf33217ea666ef
Andriy Bazyuta <tihhra@gmail.com>
2010-06-07 18:22:33 +0300

    fix returning count of record when grouping two or more columns

--- lib/will_paginate/finders/active_record.rb
+++ lib/will_paginate/finders/active_record.rb
@@ -119,7 +119,7 @@ module WillPaginate::Finders
                 counter.call
               end
 
-      count.respond_to?(:length) ? count.length : count
+      count.is_a?(ActiveSupport::OrderedHash) ? all.count : (count.respond_to?(:length) ? count.length : count)
     end
     
     def wp_parse_count_options(options, klass) #:nodoc:
